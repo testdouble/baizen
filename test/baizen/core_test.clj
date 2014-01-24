d30a4157f42587cbdf1de23712ce6473b29c2578
Matthew Boston <matthew.boston@gmail.com>
2014-01-24 13:21:19 -0500

    move test file into test-resources

--- test/baizen/core_test.clj
+++ test/baizen/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [baizen.core :refer :all]))
 
-(def test-file "resources/BAI-File-From-Bank.txt")
+(def test-file "test-resources/BAI-File-From-Bank.bai")
 
 (deftest parse-test
   (testing "reading in a file returns a clojure.lang.LazySeq"
