70195fd91ceb3998b3b21cb38ff796239eca881c
Matthew Boston <matthew.boston@gmail.com>
2014-01-24 14:50:34 -0500

    dissect the entire file header format

--- src/baizen/dissect.clj
+++ src/baizen/dissect.clj
@@ -1,9 +1,22 @@
 (ns baizen.dissect
   (:require [clojure.core.match :refer [match]]))
 
+(defn file-header
+  ""
+  [line]
+  {:record-name "File Header"
+   :sender-id (second line)
+   :receiver-id (get line 2)
+   :creation-date (get line 3)
+   :creation-time (get line 4)
+   :file-id (get line 5)
+   :record-length (get line 6)
+   :block-size (get line 7)
+   :version "2"})
+
 (defn dissect-line
   "dissect a BAI vector into a hash of understandable parts"
   [line]
   (match [line]
-         [(["01" & r] :seq) ] {:record-name "File Header"}
+         [(["01" & r] :seq) ] (file-header line)
          :else line))
--- test/baizen/dissect_test.clj
+++ test/baizen/dissect_test.clj
@@ -6,6 +6,15 @@
 
 (deftest dissect-line-test
   (testing "first line of BAI file"
-    (is (= "File Header"
-           (get (dissect-line first-line)
-                :record-name)))))
+    (let [dissected (dissect-line first-line)]
+      (is (= "File Header" (get dissected :record-name)))
+      (is (= "021000018" (get dissected :sender-id)))
+      (is (= "55287" (get dissected :receiver-id)))
+      (is (= "051025" (get dissected :creation-date)))
+      (is (= "0600" (get dissected :creation-time)))
+      (is (= "1" (get dissected :file-id)))
+      (is (= "80" (get dissected :record-length)))
+      (is (= "080" (get dissected :block-size)))
+      (is (= "2" (get dissected :version)))))
+
+  (testing "first line without optional fields"))
