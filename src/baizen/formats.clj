72c2f582336b937d2ba555c053103af33a9f990a
Matthew Boston <matthew.boston@gmail.com>
2014-01-29 22:13:05 -0500

    add docstring for ?assoc

--- src/baizen/formats.clj
+++ src/baizen/formats.clj
@@ -1,7 +1,9 @@
 (ns baizen.formats
   (:require [clojure.string :as str]))
 
-(defn ?assoc [m k v]
+(defn ?assoc
+  "only assoc if the value in the map is nil or the key doesn't exist"
+  [m k v]
   (assoc m k (or (get m k) v)))
 
 (defn drop-slash [s]
