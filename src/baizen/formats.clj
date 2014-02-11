dee8ee4f191a8b10ce3853764f11eb603685be7b
Matthew Boston <matthew.boston@gmail.com>
2014-02-11 17:12:37 -0500

    use update-in for ?assoc

--- src/baizen/formats.clj
+++ src/baizen/formats.clj
@@ -3,7 +3,7 @@
 (defn ?assoc
   "only assoc if the value in the map is nil or the key doesn't exist"
   [m k v]
-  (assoc m k (or (get m k) v)))
+  (update-in m [k] #(or %1 %2) v))
 
 (defprotocol BaiFormat
   "a clojure protocol for describing a BAI record format"
