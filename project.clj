76698c1a77a87b08d18970bf41abad31eaf5b2f6
Matthew Boston <matthew.boston@gmail.com>
2014-02-28 11:30:10 -0500

    project URL pointing to new Test Double repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject baizen "0.2.0-SNAPSHOT"
   :description "BAI file format parser"
-  :url "https://github.com/bostonaholic/baizen"
+  :url "https://github.com/testdouble/baizen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
