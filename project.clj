63628a6c24b682b8f2fccdbd7ea7bb7fb53c370b
Matthew Boston <matthew.boston@gmail.com>
2014-09-15 10:42:10 -0600

    upgrade core.match 0.2.2

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.csv "0.1.2"]
-                 [org.clojure/core.match "0.2.1"]])
+                 [org.clojure/core.match "0.2.2"]])
