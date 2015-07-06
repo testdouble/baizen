ee51196c128334045904fcca8e45e78b940ec942
Matthew Boston <matthew.boston@gmail.com>
2015-07-06 07:58:12 -0600

    test baizen on multiple clojure versions

--- project.clj
+++ project.clj
@@ -5,4 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.csv "0.1.2"]
-                 [org.clojure/core.match "0.2.2"]])
+                 [org.clojure/core.match "0.2.2"]]
+  :profiles {:dev {:resource-paths ["test-resources"]}
+             :1.5 {:resource-paths ["test-resources"]
+                   :dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:resource-paths ["test-resources"]
+                   :dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:resource-paths ["test-resources"]
+                   :dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :aliases {"test-all-profiles" ["with-profile" "dev:1.5:1.6:1.7" "test"]})
