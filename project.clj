112a16f2560c449c7325fa0fabdaefbb3e13ed21
Matthew Boston <matthew.boston@gmail.com>
2015-07-06 07:58:31 -0600

    minimum clojure version set to 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/testdouble/baizen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/core.match "0.2.2"]]
   :profiles {:dev {:resource-paths ["test-resources"]}
