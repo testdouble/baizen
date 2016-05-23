763394bf19f410c3cdedf03e343e4eae61c94292
Matthew Boston <matthew@matthewboston.com>
2016-05-23 13:39:53 -0600

    rename vbutlast to butlastv

--- src/baizen/continuations.clj
+++ src/baizen/continuations.clj
@@ -1,7 +1,7 @@
 (ns baizen.continuations
   (:require [baizen.formats :refer [drop-slash drop-trailing-whitespace]]))
 
-(defn- vbutlast
+(defn- butlastv
   "butlast for Vectors"
   [v] (subvec v 0 (dec (count v))))
 
@@ -10,17 +10,17 @@
        (re-matches #"[0-9]*" s)))
 
 (defn- combine-with-new-field [acc previous-line current-line]
-  (conj (vbutlast acc) (apply conj previous-line current-line)))
+  (conj (butlastv acc) (apply conj previous-line current-line)))
 
 (defn- drop-empty-field-and-combine [acc previous-line current-line]
-  (conj (vbutlast acc) (apply conj
-                              (vbutlast previous-line)
+  (conj (butlastv acc) (apply conj
+                              (butlastv previous-line)
                               (str (last previous-line) (last current-line))
                               (rest current-line))))
 
 (defn- space-seperate-combining-field [acc previous-line current-line]
-  (conj (vbutlast acc) (apply conj
-                              (vbutlast previous-line)
+  (conj (butlastv acc) (apply conj
+                              (butlastv previous-line)
                               (str (last previous-line) " " (last current-line))
                               (rest current-line))))
 
@@ -28,7 +28,7 @@
   "reducer which combines current 88 line with previous line or leaves the line intact"
   [acc columns]
   (if (= "88" (first columns))
-    (let [previous-line (conj (vbutlast (last acc)) (drop-slash (drop-trailing-whitespace (last (last acc)))))
+    (let [previous-line (conj (butlastv (last acc)) (drop-slash (drop-trailing-whitespace (last (last acc)))))
           line (rest columns)]
       (cond
         (only-numbers? (last previous-line)) (combine-with-new-field acc previous-line line)
