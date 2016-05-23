576a86765b9e49b052fb6f379f8121ecf79460a3
Matthew Boston <matthew@matthewboston.com>
2016-05-23 14:03:02 -0600

    use Parse protocol for top-level parsing

--- src/baizen/core.clj
+++ src/baizen/core.clj
@@ -4,23 +4,20 @@
             [baizen.continuations :as continuations]
             [baizen.parser :refer [parse-file-header parse-file-trailer parse-groups]]))
 
-(defmulti parse
-  "parse a BAI file into a Vector of Maps"
-  class)
+(defprotocol Parse
+  (parse [this] "parse a BAI file into a Vector of Maps"))
 
-(defmethod parse
+(extend-protocol Parse
   java.lang.String
-  [file-name]
-  (parse (io/reader file-name)))
+  (parse [file-name] (parse (io/reader file-name)))
 
-(defmethod parse
   java.io.Reader
-  [reader]
-  (with-open [rdr reader]
-    (let [lines (csv/read-csv rdr)
-          preprocessed (continuations/preprocess lines)
-          header (first preprocessed)
-          trailer (last preprocessed)]
-      {:file-header (parse-file-header header)
-       :groups (parse-groups (rest preprocessed))
-       :file-trailer (parse-file-trailer trailer)})))
+  (parse [reader]
+    (with-open [rdr reader]
+      (let [lines (csv/read-csv rdr)
+            preprocessed (continuations/preprocess lines)
+            header (first preprocessed)
+            trailer (last preprocessed)]
+        {:file-header (parse-file-header header)
+         :groups (parse-groups (rest preprocessed))
+         :file-trailer (parse-file-trailer trailer)}))))
